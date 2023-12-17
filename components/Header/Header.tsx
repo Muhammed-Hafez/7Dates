7d447c4cd1dfc35e3aef3c234d10785f341c4535
Ali Samir <alisamir.eng@gmail.com>
2023-12-17 16:18:51 +0200

    dev

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,20 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between md:px-24 md:py-5">
-      <Image
-        src="/images/logo.png"
-        alt="Seven Dates"
-        width={200}
-        height={200}
-      />
+      <Link href="/">
+        <Image
+          src="/images/logo.png"
+          alt="Seven Dates"
+          width={200}
+          height={200}
+        />
+      </Link>
 
       <nav className="flex items-center justify-center gap-5">
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/products">Products</a>
-        <a href="/contact">Contact</a>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/products">Products</Link>
+        <Link href="/contact">Contact</Link>
       </nav>
     </header>
   );
