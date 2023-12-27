c1c09d08eba1f520815523bcaf9c1e16850f83d4
Ali Samir <alisamir.eng@gmail.com>
2023-12-27 16:42:29 +0200

    dev

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -25,7 +25,7 @@ const AboutPage = () => {
         </div>
 
         <div className={styles.story__img}>
-          <img src="/images/products/1.webp" alt="About" />
+          <img src="/images/products/Saudi.jpg" alt="About" />
         </div>
       </section>
 
@@ -40,7 +40,7 @@ const AboutPage = () => {
         </div>
 
         <div className={styles.vision__img}>
-          <img src="/images/products/3.webp" alt="About" />
+          <img src="/images/products/Safawy.jpg" alt="About" />
         </div>
       </section>
 
@@ -56,7 +56,7 @@ const AboutPage = () => {
         </div>
 
         <div className={styles.mission__img}>
-          <img src="/images/products/5.webp" alt="About" />
+          <img src="/images/products/Ajwa.jpg" alt="About" />
         </div>
       </section>
 
