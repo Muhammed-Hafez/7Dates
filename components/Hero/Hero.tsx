016b07f23087a44cdf2d9ea23436d245f281b46a
Ali Samir <alisamir.eng@gmail.com>
2023-12-28 13:29:51 +0200

    dev

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import "@splidejs/react-splide/css/core";
-import React from "react";
-
+import "@splidejs/splide/dist/css/splide.min.css";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 
 import styles from "./Hero.module.scss";
@@ -11,20 +10,26 @@ const sliderImages = [
   {
     id: 1,
     image: "/images/slider-images/slide-1.jpg",
+    quote:
+      "“Savor the sweetness of life with 7Dates – where every bite is a journey into the heart of tradition.”",
   },
   {
     id: 2,
     image: "/images/slider-images/slide-2.jpg",
+    quote:
+      "“Empower your brand with the richness of 7Dates - where tradition meets innovation, and quality becomes an enduring partnership.”",
   },
   {
     id: 3,
     image: "/images/slider-images/slide-3.jpg",
+    quote:
+      "“Journey through the oasis of flavor – 7 Dates, where the authenticity of tradition meets the innovation of excellence.”",
   },
 ];
 
-function HeroCarousel() {
+function Hero() {
   return (
-    <section className="w-full h-[800px] mx-auto relative overflow-hidden">
+    <section>
       <Splide
         options={{
           arrows: false,
@@ -39,11 +44,11 @@ function HeroCarousel() {
         {sliderImages.map((slide) => (
           <SplideSlide key={slide.id}>
             <div className={styles.img__container}>
-              <img
-                src={slide.image}
-                alt="slider image"
-                className="w-[100%] h-[100%] object-cover object-bottom"
-              />
+              <img src={slide.image} alt="slider image" />
+              <div className={styles.overlay}></div>
+              <div className={styles.hero__quote}>
+                <h3>{slide.quote}</h3>
+              </div>
             </div>
           </SplideSlide>
         ))}
@@ -52,4 +57,4 @@ function HeroCarousel() {
   );
 }
 
-export default HeroCarousel;
+export default Hero;
