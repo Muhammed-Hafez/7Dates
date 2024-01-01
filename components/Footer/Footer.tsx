ffbff0a952e9fac5ec0c55d35c0ce08a464f6f8a
Ali Samir <alisamir.eng@gmail.com>
2024-01-01 14:53:31 +0200

    dev

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -8,33 +8,31 @@ import styles from "./Footer.module.scss";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#e0af7f30]">
-      <div className="w-full max-w-[1400px] mx-auto py-8 overflow-hidden">
-        <div className="flex flex-col items-center justify-center">
-          <div className="mb-5">
-            <p className="text-sm font-semibold tracking-wider text-center">
-              Seven Dates Factory Packing and Processing Dates LLC © 2024
-            </p>
-          </div>
-
-          <ul className="flex items-center justify-center gap-3">
-            <li>
-              <Link href="/">
-                <FaFacebook size={25} color={"#e0b07f"} />
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <FaInstagram size={25} color={"#e0b07f"} />
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <BsTwitterX size={25} color={"#e0b07f"} />
-              </Link>
-            </li>
-          </ul>
+    <footer className={styles.footer}>
+      <div className={styles.footer__container}>
+        <div className="mb-5">
+          <p className="text-lg text-center text-white">
+            Seven Dates Factory Packing and Processing Dates LLC © 2024
+          </p>
         </div>
+
+        <ul className="flex items-center justify-center gap-3">
+          <li>
+            <Link href="/">
+              <FaFacebook size={25} color={"#fff"} />
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <FaInstagram size={25} color={"#fff"} />
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <BsTwitterX size={25} color={"#fff"} />
+            </Link>
+          </li>
+        </ul>
       </div>
     </footer>
   );
