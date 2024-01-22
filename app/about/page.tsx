4980db90c985f5f48f2e00ca9f2e1cf7b964c9ac
Ali Samir <alisamir.eng@gmail.com>
2024-01-22 14:34:03 +0200

    Responsive

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,58 +7,60 @@ const AboutPage = () => {
     <>
       <section className={styles.about}></section>
 
-      <section className={styles.about__content}>
-        <h1>About Us</h1>
-        <p>The success history of 7 Dates in 18 years</p>
-      </section>
+      <div className={styles.about__container}>
+        <section className={styles.about__content}>
+          <h1>About Us</h1>
+          <p>The success history of 7 Dates in 18 years</p>
+        </section>
 
-      <section className={styles.about__story}>
-        <div className={styles.story__content}>
-          <h2>Our Story</h2>
-          <p>
-            With a legacy spanning over 18 years, Seven Dates Factory Packing
-            and Processing Dates LLC has been a pioneer in sourcing and packing
-            the finest date products. Our journey is rooted in a dedication to
-            quality, customer satisfaction, and a passion for delivering the
-            best of the UAE's date varieties.
-          </p>
-        </div>
+        <section className={styles.about__story}>
+          <div className={styles.story__content}>
+            <h2>Our Story</h2>
+            <p>
+              With a legacy spanning over 18 years, Seven Dates Factory Packing
+              and Processing Dates LLC has been a pioneer in sourcing and
+              packing the finest date products. Our journey is rooted in a
+              dedication to quality, customer satisfaction, and a passion for
+              delivering the best of the UAE's date varieties.
+            </p>
+          </div>
 
-        <div className={styles.story__img}>
-          <img src="/images/products/Saudi.png" alt="About" />
-        </div>
-      </section>
+          <div className={styles.story__img}>
+            <img src="/images/products/Saudi.jpg" alt="About" />
+          </div>
+        </section>
 
-      <section className={styles.about__vision}>
-        <div className={styles.vision__content}>
-          <h2>Vision</h2>
-          <p>
-            At Seven Dates, our vision is to be the leading provider of
-            high-quality date products globally, ensuring convenience,
-            excellence, and a touch of tradition in every date
-          </p>
-        </div>
+        <section className={styles.about__vision}>
+          <div className={styles.vision__content}>
+            <h2>Vision</h2>
+            <p>
+              At Seven Dates, our vision is to be the leading provider of
+              high-quality date products globally, ensuring convenience,
+              excellence, and a touch of tradition in every date
+            </p>
+          </div>
 
-        <div className={styles.vision__img}>
-          <img src="/images/products/Safawy.png" alt="About" />
-        </div>
-      </section>
+          <div className={styles.vision__img}>
+            <img src="/images/products/Safawy.jpg" alt="About" />
+          </div>
+        </section>
 
-      <section className={styles.about__mission}>
-        <div className={styles.mission__content}>
-          <h2>Mission</h2>
-          <p>
-            Our mission is to deliver the finest dates to UAE nationals,
-            residents, and the global community, leveraging our 18 years of
-            experience, cutting-edge technology, and a commitment to customer
-            satisfaction
-          </p>
-        </div>
+        <section className={styles.about__mission}>
+          <div className={styles.mission__content}>
+            <h2>Mission</h2>
+            <p>
+              Our mission is to deliver the finest dates to UAE nationals,
+              residents, and the global community, leveraging our 18 years of
+              experience, cutting-edge technology, and a commitment to customer
+              satisfaction
+            </p>
+          </div>
 
-        <div className={styles.mission__img}>
-          <img src="/images/products/Ajwa.png" alt="About" />
-        </div>
-      </section>
+          <div className={styles.mission__img}>
+            <img src="/images/products/Ajwa.jpg" alt="About" />
+          </div>
+        </section>
+      </div>
 
       <Clients />
       <ContactInfo />
